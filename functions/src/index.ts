7fd8145499c3338559d33b0ec40c160338603352
Tal Yaron <tal.yaron@gmail.com>
2022-09-23 17:06:46 +0300

    we can count votes now! -> to check for new vote and delete options

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,9 @@
+import { runTransaction } from "firebase/firestore";
 import * as functions from "firebase-functions";
-import admin = require('firebase-admin');
+import admin = require("firebase-admin");
+import { get } from "lodash";
 admin.initializeApp();
+const db = admin.firestore();
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -10,11 +13,56 @@ admin.initializeApp();
 //   response.send("Hello from Firebase!");
 // });
 
+exports.makeUppercase = functions.firestore
+  .document("/messages/{documentId}")
+  .onCreate((snap, context) => {
+    const original = snap.data().original;
+    console.log("Uppercasing", context.params.documentId, original);
+    const uppercase = original.toUpperCase();
+    return snap.ref.set({ uppercase }, { merge: true });
+  });
 
-exports.makeUppercase = functions.firestore.document('/messages/{documentId}')
-.onCreate((snap, context) => {
-  const original = snap.data().original;
-  console.log('Uppercasing', context.params.documentId, original);
-  const uppercase = original.toUpperCase();
-  return snap.ref.set({uppercase}, {merge: true});
-});
+exports.updateVoteToOption = functions.firestore
+  .document("/counsils/{counsilId}/votes/{userUid}")
+  .onWrite((change, context) => {
+    try {
+      const { counsilId } = context.params;
+      console.log("counsilId", counsilId);
+      // if(!change.before.data())throw new Error('No CHANGE was detected');
+
+      const beforeOption = change.before.data().vote;
+      const afterOption = change.after.data().vote;
+      console.log("vote before:", beforeOption);
+      console.log("change after:", afterOption);
+
+      const afterOptionRef = db
+        .collection("counsils")
+        .doc(counsilId)
+        .collection("options")
+        .doc(afterOption);
+
+      const beforeOptionRef = db
+        .collection("counsils")
+        .doc(counsilId)
+        .collection("options")
+        .doc(beforeOption);
+
+      return db.runTransaction(async (transaction) => {
+        //after
+        const optionSelected = await transaction.get(afterOptionRef);
+        const optionLeave = await transaction.get(beforeOptionRef);
+
+        //selected votes
+        const votesBeforeSelected = get(optionSelected.data(), "votes", 0);
+        const newVotes = votesBeforeSelected + 1;
+        transaction.update(afterOptionRef, { votes: newVotes });
+
+        //votes afler leave
+        const votesBeforeLeave = get(optionLeave.data(), "votes", 1);
+        const newVotesLeave = votesBeforeLeave - 1;
+        transaction.update(beforeOptionRef, { votes: newVotesLeave });
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  });
--- src/features/selections/votes/setVote.ts
+++ src/features/selections/votes/setVote.ts
@@ -6,12 +6,9 @@ import { User } from "../../user/userModel";
 export async function setVote(councilId:string, optionId:string, user:User){
     try {
         console.log(user)
-        const voteRef = doc(DB,Collections.COUNSILS,councilId,Collections.VOTES,'votes');
-        const vote:any = {};
-        vote[user.uid] = optionId;
-
-
-        await setDoc(voteRef,vote,{merge:true});
+        const voteRef = doc(DB,Collections.COUNSILS,councilId,Collections.VOTES,user.uid);
+       
+        await setDoc(voteRef,{vote:optionId},{merge:true});
     } catch (error) {
         console.error(error)
     }
