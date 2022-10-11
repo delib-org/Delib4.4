21c793c9e07fe25f7a2637525b652478f6c1b4aa
Tal Yaron <tal.yaron@gmail.com>
2022-10-11 13:26:07 +0300

    we can see voting changes

--- src/features/options/view/OptionBar.tsx
+++ src/features/options/view/OptionBar.tsx
@@ -3,13 +3,14 @@ import { OptionProps } from "../model/optionModel";
 
 interface OptionBarProps {
   option: OptionProps;
+  maxVotes:number;
 }
 
-const OptionBar: FC<OptionBarProps> = ({ option }) => {
+const OptionBar: FC<OptionBarProps> = ({ option,maxVotes}) => {
   return (
     <div
       className="optionsBar__bar"
-      style={{ height: `${option.votes * 10}vh` }}>
+      style={{ height: `${(option.votes/maxVotes)*100}%` }}>
       {option.votes}
     </div>
   );
--- src/features/options/view/OptionsBars.tsx
+++ src/features/options/view/OptionsBars.tsx
@@ -13,6 +13,8 @@ const OptionsBars: FC<OptionsBarsProps> = ({ counsilId }) => {
   const options = useAppSelector((state) =>
     state.options.options.filter((option) => option.counsilId === counsilId)
   );
+  // const maxVotes = Math.max(...options.map(o => o.votes));
+  const maxVotes:number = options.reduce((prv,cur)=>prv + cur.votes, 0);
 
   function handleAddOption() {
     console.log(showAddOption);
@@ -22,10 +24,11 @@ const OptionsBars: FC<OptionsBarsProps> = ({ counsilId }) => {
   return (
     <div className="optionsBar">
       <h3>OptionsBars</h3>
+      <div>Total Votes:{maxVotes}</div>
       <button onClick={handleAddOption}>ADD OPTION</button>
       <div className="optionsBar__grid" style={{gridTemplateColumns:`repeat(${options.length},1fr)`}}>
         {options.map((option) => (
-          <OptionBar key={`${option.optionId}-bar`} option={option} />
+          <OptionBar key={`${option.optionId}-bar`} option={option} maxVotes={maxVotes}/>
         ))}
 
         {options.map((option: OptionProps) => (
