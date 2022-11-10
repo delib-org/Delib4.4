baeae0f9d1cdfe30b8172421aafa39dff8476351
Tal Yaron <tal.yaron@gmail.com>
2022-11-10 10:30:14 +0200

    we have indection on the section

--- src/features/council/CouncilMenu.tsx
+++ src/features/council/CouncilMenu.tsx
@@ -1,12 +1,30 @@
 import React from "react";
 import { Link, useParams } from "react-router-dom";
+import { OptionsView } from "../options/model/optionModel";
 
 const CouncilMenu = () => {
   const { counsilId, section } = useParams();
+  console.log("CouncilMenu", section);
   return (
     <div className="councilMenu">
-      <Link to={`/counsil/${counsilId}`}><div className="councilMenu__btn">VOTES</div></Link>
-      <Link to={`/counsil/${counsilId}/board`}><div className="councilMenu__btn">BOARD</div></Link>
+      <Link
+        to={`/counsil/${counsilId}`}
+        className={
+          section === OptionsView.BARS || section === undefined
+            ? "councilMenu__btn councilMenu__btn--selected"
+            : "councilMenu__btn"
+        }>
+        VOTES
+      </Link>
+      <Link
+        to={`/counsil/${counsilId}/board`}
+        className={
+          section === OptionsView.BOARD
+            ? "councilMenu__btn councilMenu__btn--selected"
+            : "councilMenu__btn"
+        }>
+        BOARD
+      </Link>
     </div>
   );
 };
