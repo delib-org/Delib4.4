1ff6d34154752e6a0d54f9b5f9441c5f96bad0fe
Tal Yaron <tal.yaron@gmail.com>
2023-02-10 11:07:53 +0200

    WIP

--- src/features/council/CouncilMenu.tsx
+++ src/features/council/CouncilMenu.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Link, useParams } from "react-router-dom";
 import { OptionsView } from "../options/model/optionModel";
+import ChatIcon from "@mui/icons-material/Chat";
+import BarChartIcon from "@mui/icons-material/BarChart";
 
 const CouncilMenu = () => {
   const { councilId, section } = useParams();
- 
+
   return (
     <div className="councilMenu">
       <Link
@@ -14,7 +16,12 @@ const CouncilMenu = () => {
             ? "councilMenu__btn councilMenu__btn--selected"
             : "councilMenu__btn"
         }>
-        VOTES
+        <div className="councilMenu__text">
+          <div className="councilMenu__text__text">VOTES</div>
+          <div className="councilMenu__text__icon">
+            <BarChartIcon />
+          </div>
+        </div>
       </Link>
       <Link
         to={`/council/${councilId}/board`}
@@ -23,7 +30,12 @@ const CouncilMenu = () => {
             ? "councilMenu__btn councilMenu__btn--selected"
             : "councilMenu__btn"
         }>
-        DISCUSSION
+        <div className="councilMenu__text">
+          <div className="councilMenu__text__text">DISCUSSION</div>
+          <div className="councilMenu__text__icon">
+            <ChatIcon />
+          </div>
+        </div>
       </Link>
     </div>
   );
